9c6eddd52ca702dc0cbfc33ce597d02a77211734
Saidakbar693 <jamoliddinovsaidakbar@gmail.com>
2021-07-28 12:56:04 +0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+// //rfce
+import React from 'react'
+import './Header.css';
+function Header() {
+    return (
+        <div className = "header">
+        <ul className = "ul it"> 
+         <li><a href="#"><img src="#"></img></a></li> 
+         <li><a href="#">Mac</a></li> 
+         <li><a href="#">iPad</a></li> 
+         <li><a href="#">iPhone</a></li> 
+         <li><a href="#">Watch</a></li> 
+         <li><a href="#">TV</a></li>
+         <li><a href="#">Music</a></li>
+         <li><a href="#">Support</a></li>
+        </ul>
+        </div>
+    )
+}
+
+export default Header
